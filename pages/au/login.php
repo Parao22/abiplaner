6cf98d5af37994d2b18bbb636ac3c26deed85d6a
Parao22 <133631592+Parao22@users.noreply.github.com>
2023-06-05 09:32:27 +0200

    comit

--- pages/au/login.php
+++ pages/au/login.php
@@ -1,5 +1,54 @@
 <?php
+    session_start([
+        'cookie_lifetime' => 86400,
+    ]);
     $email = $_POST['email'];
     $password = $_POST['password'];
-    echo 'EMail: '.$email.'<br>';
-    echo 'Passwort: '.$password;
+
+    
+$servername = "localhost"; 
+$username = "abiplaner"; 
+$password = "Q1ShlM_abiplaner"; 
+$dbname = "abiplaner"; 
+// Create connection 
+
+$conn = new mysqli($servername, $username, $password, $dbname); 
+
+// Check connection 
+
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
+
+$result = $conn->query("SELECT password FROM user where email ='". $email."'"); 
+
+// Check ob es Ergebnisse in der Abfrage gab
+if ($result->num_rows > 0) {
+    //Nehme das erste Ergebnis aus der SQL Abfrage
+    $hashed_password = $result->fetch_assoc();
+    if ( password_verify($password, $hashed_password) ) {
+    // Passwort war richtig.
+        if( password_needs_rehash($hashed_password, PASSWORD_DEFAULT) ) {
+            /*  Der Hashalgorithmus des gespeicherten Passworts genügt nicht mehr
+            *  den aktuellen Anforderungen, daher sollte es mittels password_hash()
+            *  neu gehasht und anstelle des alten Hashes in der Datenbank gespeichert
+            *  werden; hier wird es nur in der entsprechenden Variable geändert:
+            */
+            $hashed_password = password_hash($password,  PASSWORD_DEFAULT);
+            // ToDo: neu gehashtes Passwort in DB speichern!
+        }
+
+        //Session für Nutzer erstellen
+        $_SESSION['user_mail'] = $email;
+
+        //Weiterleitung
+        echo "LOGIN ERFOLGREICH";
+    } else {
+    // Passwort war falsch.
+    // Zurück mit Fehlermeldung
+    echo "PASSWORT FALSCH";
+    }
+}else{
+    // EMail war falsch.
+    echo "EMAIL FALSCH";
+}
