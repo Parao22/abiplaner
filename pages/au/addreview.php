a251035f8b7e329766df97b96f2973d25541810d
Parao22 <133631592+Parao22@users.noreply.github.com>
2023-07-05 23:00:25 +0200

    ssa

--- pages/au/addreview.php
+++ pages/au/addreview.php
@@ -1,8 +1,15 @@
 <?php
+session_start();
+if(!isset($_SESSION['user_name'])){
+    header('Location: ../login.html');
+    exit();
+}
 print_r($_POST);
+print_r($_SESSION['user_name']);
 $ku_name = $_POST['Kursname'];
 $lk1 = $_POST['LK'][0];
 $lk2 = $_POST['LK'][1];
+$us_name = $_SESSION['user_name'];
 $servername = "localhost"; 
 $username = "abiplaner"; 
 $password = "Q1ShlM_abiplaner"; 
@@ -18,6 +25,9 @@ if ($conn->connect_error) {
 } 
 
 $conn->query("INSERT INTO kurswahl (name) VALUES ('{$ku_name}')");
-$conn->query("INSERT INTO erstellen (username,kurswahl_name) VALUES ('','{$ku_name}')");
+$conn->query("INSERT INTO erstellen (username,kurswahl_name) VALUES ('{$us_name}','{$ku_name}')");
 $conn->query("INSERT INTO besteht (kurswahl_name,subject_name) VALUES ('{$ku_name}','{$lk1}')");
 $conn->query("INSERT INTO besteht (kurswahl_name,subject_name) VALUES ('{$ku_name}','{$lk2}')");
+
+header('Location: gkKurswahl.php');
+exit();
